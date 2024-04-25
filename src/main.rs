0807eb34d2bedd6db2107e0a8a05a0dfaf2b4cf9
kaka <hachimi-lab@outlook.com>
2024-04-25 20:02:28 +0800

    读取CSV文件

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Ok, Result};
+use serde::{Deserialize, Serialize};
+use std::fs::File;
+
+#[allow(dead_code)]
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Player {
+    #[serde(rename = "Name")]
+    pub name: String,
+    #[serde(rename = "Position")]
+    pub position: String,
+    #[serde(rename = "DOB")]
+    pub dob: String,
+    #[serde(rename = "Nationality")]
+    pub nationality: String,
+    #[serde(rename = "Kit Number")]
+    pub number: u8,
+}
+
+impl Player {
+    pub fn to_json(&self) -> Result<String> {
+        let json = serde_json::to_string(&self)?;
+        Ok(json)
+    }
+    pub fn from_json(json: &str) -> Result<Player> {
+        let player: Player = serde_json::from_str(json)?;
+        Ok(player)
+    }
+}
+
+fn main() -> Result<()> {
+    let file = File::open("assets/juventus.csv")?;
+    let mut reader = csv::ReaderBuilder::new()
+        .has_headers(true)
+        .from_reader(file);
+    for result in reader.deserialize() {
+        let player: Player = result?;
+        println!("Player:{}", player.to_json()?);
+    }
+    Ok(())
 }
